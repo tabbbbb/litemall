37c5a3a719c669ef8dbdc078dc3512599d99a85b
caozhi <1310840185@qq.com>
2019-12-02 11:21:07 +0800

    修改了用户足迹

--- litemall-admin-api/src/main/java/com/lhcode/litemall/admin/Application.java
+++ litemall-admin-api/src/main/java/com/lhcode/litemall/admin/Application.java
@@ -15,7 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args); //dd
     }
 
 }
