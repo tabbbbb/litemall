1181f9a8b626d480cd92855e972e515f97c29bb6
caozhi <1310840185@qq.com>
2019-12-04 09:19:54 +0800

    修改了用户足迹

--- litemall-db/src/main/java/com/lhcode/litemall/db/service/LitemallShoppingTrolleyService.java
+++ litemall-db/src/main/java/com/lhcode/litemall/db/service/LitemallShoppingTrolleyService.java
@@ -41,6 +41,7 @@ public class LitemallShoppingTrolleyService {
         LitemallShoppingTrolleyExample.Criteria criteria = example.createCriteria();
         criteria.andGoodsIdEqualTo(trolley.getGoodsId());
         criteria.andGoodsSpecIdEqualTo(trolley.getGoodsSpecId());
+        criteria.andGoodsSpecIdEqualTo(trolley.getUserId());
         List<LitemallShoppingTrolley> shoppingTrolleyList = shoppingTrolleyMapper.selectByExample(example);
         if (shoppingTrolleyList.size() != 0){
             LitemallShoppingTrolley shoppingTrolley = shoppingTrolleyList.get(0);
